012aa31e7db06166d415c3e1678ed65f746c901f
Samupietila <122264320+Samupietila@users.noreply.github.com>
2023-03-30 15:54:08 +0300

    1.8

---
+++ 1_8.js
@@ -0,0 +1,18 @@
+'use strict';
+let Aloitusvuosi = parseInt(prompt('Anna aloitusvuosi:'));
+const Lopetusvuosi = parseInt(prompt('Anna lopetusvuosi:'));
+while (Aloitusvuosi % 4 !== 0) {
+  Aloitusvuosi++;
+}
+const x = document.createElement('li');
+let y = document.createTextNode(Aloitusvuosi);
+x.appendChild(y);
+const element = document.getElementById('ul');
+element.appendChild(x);
+do {
+  Aloitusvuosi += 4
+  const x = document.createElement('li');
+  let y = document.createTextNode(Aloitusvuosi);
+  x.appendChild(y);
+  element.appendChild(x);
+} while (Aloitusvuosi < Lopetusvuosi);
